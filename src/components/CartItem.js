143b1e2d3ef2338a47d1e2989c3314b2609b45a7
haruki suziuki <harukiti166@gmail.com>
2023-05-30 13:43:26 +0900

    first commit

---
+++ src/components/CartItem.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { PlusIcon } from "../HeroIcons";
+import { useDispatch } from "react-redux";
+import { removeItem, increase, decrease } from "../features/cart/CartSlice";
+
+const CartItem = (props) => {
+  const dispatch = useDispatch();
+  const { id, img, title, price, amount } = props;
+  return (
+    <div>
+      <article className="cart-item">
+        <img src={img} alt="" />
+        <div>
+          <h4>{title}</h4>
+          <h4 className="item-price">{price}円</h4>
+          <button
+            className="remove-btn"
+            onClick={() => dispatch(removeItem(id))}
+          >
+            削除
+          </button>
+        </div>
+        <div>
+          <button className="amount-btn" onClick={() => dispatch(increase(id))}>
+            <PlusIcon />
+          </button>
+          <p className="amount">{amount}</p>
+          <button
+            className="amount-btn"
+            onClick={() => {
+              if (amount === 1) {
+                dispatch(removeItem(id));
+                return;
+              }
+              dispatch(decrease(id));
+            }}
+          >
+            <PlusIcon />
+          </button>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default CartItem;
